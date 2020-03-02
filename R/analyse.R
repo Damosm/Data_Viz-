dea5776e0a4521f3e3a6c519f22642c17c921e9d
Jaks <jcqs.gr@gmail.com>
2020-03-02 15:36:04 +0100

    maj repo RAZ

---
+++ R/analyse.R
@@ -0,0 +1,7 @@
+data_convention <- read.csv("C:/Users/Utilisateur/Desktop/Data_Viz-/data/declaration_convention_2020_02_19_04_00.csv",sep =";", encoding= "utf-8")
+data_avantage <- read.csv("C:/Users/Utilisateur/Desktop/Data_Viz-/data/declaration_avantage_2020_02_19_04_00.csv",sep =";", encoding= "utf-8")
+data_remuneration <- read.csv("C:/Users/Utilisateur/Desktop/Data_Viz-/data/declaration_remuneration_2020_02_19_04_00.csv",sep =";", encoding= "utf-8")
+data_entreprise <- read.csv("C:/Users/Utilisateur/Desktop/Data_Viz-/data/entreprise_2020_02_19_04_00.csv",sep =",", encoding= "utf-8") 
+
+
+table(data_remuneration)
--- main.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('db/remuneration.sqlite')
-c = conn.cursor()
-
-c.execute("SELECT * FROM MLS;")
-print(c.fetchall())
