4afa5330ced20659c92b468c08b40eb654d8b78a
Jaks <jcqs.gr@gmail.com>
2020-03-02 10:54:44 +0100

    updating remuneration.py

--- remuneration.py
+++ remuneration.py
@@ -1,6 +1,10 @@
 from classes.Csv import Csv
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite://', echo=False)
 
 remuneration_csv = Csv('declaration_remuneration_2020_02_19_04_00', 'utf-8', ';')
 df = remuneration_csv.get_data()
+df.to_sql('remuneration', con=engine)
 
-print(df.iloc[0])
+# print(engine.execute("SELECT * FROM remuneration").fetchall())
